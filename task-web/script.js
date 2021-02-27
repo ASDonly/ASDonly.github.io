f341fbb5334aae52337acc1276d0082f3d282ad6
ASDonly <78333152+ASDonly@users.noreply.github.com>
2021-02-28 01:24:38 +0800

    Add files via upload

---
+++ task-web/script.js
@@ -0,0 +1,32 @@
+/*JavaScript*/
+
+
+
+
+function menulistclick(){
+    var list=document.getElementById("list");
+    if(list.style.display=="none"){
+        list.style.display="block";
+        list.style.animationName="list-on";
+        list.style.animationDuration="0.2s";
+        
+    }else{
+        list.style.animationName="list-off";
+        list.style.animationDuration="0.2s";
+        setTimeout(function(){
+            list.style.display="none";
+        },100);
+    }
+}
+
+function homelink(){
+    location.href="index.html";
+}
+
+function tasklink(){
+    location.href="task.html";
+}
+
+function shoplink(){
+    location.href="shop.html";
+}
